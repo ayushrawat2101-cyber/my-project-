c090dde5818ba0ee29031bbcea48f78176858698
ayushrawat <ayushrawat2101@gamil.com>
2026-02-11 00:12:54 +0530

    operator

---
+++ moduloop.java
@@ -0,0 +1,23 @@
+public class moduloop {
+    public static void main(String[] args) {
+    // int a = 4 ;
+    // int b = 7;
+    // int c = a%b;
+    // System.out.print (c);
+    // case  1 a<b
+    /*int a = 4 ;
+    int b = -7 ;
+    int c = a % b ;
+    System.out .print (c );*/
+    // case 2 when a % (-b)= a%b
+
+    // case 3 when (-a )%b = -(a%b)
+    // int a = -39 ;
+    // int b =10 ;
+    // int c = a % b ;
+    // System .out . print (c );
+
+
+
+    }
+}
