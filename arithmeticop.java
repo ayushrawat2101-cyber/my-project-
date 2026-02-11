84428cf248113263c0e1a6b1ed186b80a2f83391
ayushrawat <ayushrawat2101@gmail.com>
2026-02-11 15:04:54 +0530

    arithmetic

--- arithmeticop.java
+++ arithmeticop.java
@@ -1,13 +1,13 @@
- public class arithmeticop {
-     public static void main(String[] args ){
-         int x = 5 ,y=2 ;
-         System.out.println(x+y );
- System.out.println(x-y );
- System.out.println(x*y );
-         System.out.println(x/y );
-     }
+//  public class arithmeticop {
+//      public static void main(String[] args ){
+//          int x = 5 ,y=2 ;
+//          System.out.println(x+y );
+//  System.out.println(x-y );
+//  System.out.println(x*y );
+//          System.out.println(x/y );
+//      }
     
- }
+//  }
 public class arithmeticop{
     public static void main(String[] args ){
 double x = 5;
