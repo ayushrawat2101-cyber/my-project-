4c6941b9fa692743c65f529c76dd5745ccff3be8
ayushrawat <ayushrawat2101@gmail.com>
2026-02-11 15:06:24 +0530

    arear

--- areaofcircle.java
+++ areaofcircle.java
@@ -1,12 +1,12 @@
 public class areaofcircle {
     public static void main(String[] args ){
-         double radius = 5 ;
-         double pi = 3.1415 ;
-         double area = pi * radius * radius  ;
-         System.out . println(area);
+        //  double radius = 5 ;
+        //  double pi = 3.1415 ;
+        //  double area = pi * radius * radius  ;
+        //  System.out . println(area);
         
 
-         volume of sphere of radius = 5 
+        // volume of sphere of radius = 5 
 
 
          double radius = 5;
@@ -14,7 +14,7 @@ public class areaofcircle {
          double volume = 4 * pi *radius * radius * radius / 3 ;
          System.out.println(volume );
 
-         t.s.a of cuboid 
+        // t.s.a of cuboid 
     int length = 10 ;
     int breadth = 9;
     int height = 3;
