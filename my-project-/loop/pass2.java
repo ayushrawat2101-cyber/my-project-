b05664991b723685c816d84004616ffc9f383410
aysuhrawat <ayushrawat2101@gmail.com>
2026-04-03 08:11:00 +0530

    basics

---
+++ my-project-/loop/pass2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class pass2 {
+    public static void main(String[] args ){
+        Scanner sc = new Scanner ( System.in );
+       int password ;
+        do {
+            System.out.println("enter the password ");
+  password = sc.nextInt();
+        }while ( password == 5678);
+    }
+}
